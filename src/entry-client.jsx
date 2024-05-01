39a671ca4ea110968b23fd83fd576fe83818b5c4
fireFistAniket <sahaaniket720@gmail.com>
2024-05-01 07:45:30 +0530

    first commit

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,11 +1,14 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root'),
+  document.getElementById("root"),
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
