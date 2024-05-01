4d59b798940350f5af0b57fbf01464cff290e622
fireFistAniket <sahaaniket720@gmail.com>
2024-05-01 08:31:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/virtualparadise/",
-  server: {
-    cors: false,
-    proxy: {
-      "/api": {
-        target: "https://api.igdb.com/v4",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        headers: {
-          "Client-ID": "afpb4kqzcdyb8k79dlwimcaxuabgk3",
-          Authorization: "Bearer 5oehm8u6ueavv4rxi67psiossu1q72",
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: "/virtualparadise/",
+    server: {
+      cors: false,
+      proxy: {
+        "/api": {
+          target: "https://api.igdb.com/v4",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          headers: {
+            "Client-ID": env.VITE_SERVER_CLIENT_ID,
+            Authorization: `Bearer ${env.VITE_SERVER_ACCESS_TOKEN}`,
+          },
         },
       },
     },
-  },
+  };
 });
