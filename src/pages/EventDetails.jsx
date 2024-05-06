0013a6c403535e4fbb2a51dfa611db2a6c1f0489
fireFistAniket <sahaaniket720@gmail.com>
2024-05-06 08:29:47 +0530

    event details complete, robots.txt added

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Loader from "../components/Loader";
+import ReactPlayer from "react-player/lazy";
 
 const EventDetails = () => {
-  let { eventId } = useParams();
+  const { eventId } = useParams();
+  const [eventDetails, setEventDetails] = useState({});
   const {
     data,
     error: eventsError,
     loading: eventsLoading,
   } = useFetch(
     `/api/events`,
-    `fields *, event_logo.*, games.*, videos.*; where id = ${eventId};`
+    `fields *, event_logo.*, games.*, videos.*, games.cover.*; where id = ${eventId};`
   );
-  let eventDetails;
   useEffect(() => {
     if (!data) {
       return;
     }
-    eventDetails = data[0];
+    setEventDetails(data[0]);
   }, [data]);
-  if (!eventDetails && eventsLoading) {
+
+  if (eventsLoading) {
     return <Loader />;
   }
   return (
     <main className="min-h-[80vmin] flex flex-col gap-[5vmin] my-[1.4vmin]">
-      <div className="flex items-center justify-center min-h-[50vmin]">
+      <div className="flex items-center justify-center">
         <img
           src={`${import.meta.env.VITE_IMAGE_URI}/${
             eventDetails?.event_logo?.image_id
@@ -33,7 +35,67 @@ const EventDetails = () => {
           alt="event_cover"
           width={1920}
           height={1080}
+          className="max-h-[70vmin] w-full"
         />
+      </div>
+      <div className="mx-[3vmax] flex flex-col items-center gap-[3vmin]">
+        <div className="flex flex-col gap-[2vmin]">
+          <h1 className="text-neutral-100 text-[2.5vmax] font-bold">
+            {eventDetails.name}
+          </h1>
+          <p className="text-neutral-100 text-[1.6vmax] font-medium">
+            {eventDetails.description}
+          </p>
+        </div>
+        <ReactPlayer
+          url={eventDetails.live_stream_url}
+          width={800}
+          height={600}
+        />
+        <div className="flex flex-col items-start gap-[2vmin]">
+          <h2 className="text-[2vmax] text-neutral-100 font-semibold">
+            Launched Games at {eventDetails.name}
+          </h2>
+          <div
+            className={`flex items-center gap-[2vmax] flex-wrap justify-between`}
+          >
+            {eventDetails?.games?.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col items-center relative"
+              >
+                <img
+                  src={`${import.meta.env.VITE_IMAGE_URI}/${
+                    item.cover.image_id
+                  }.jpg`}
+                  alt="game cover"
+                  width={400}
+                  height={250}
+                  className="max-w-[15vmax]"
+                />
+                <p className="text-[1.5vmax] font-bold absolute bottom-0 bg-black bg-opacity-50 text-neutral-100 text-center w-full">
+                  {item.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col items-start gap-[2vmin]">
+          <h2 className="text-[2vmax] text-neutral-100 font-semibold">
+            Videos of {eventDetails.name}
+          </h2>
+          <div
+            className={`flex items-center gap-[2vmax] flex-wrap justify-between`}
+          >
+            {eventDetails.videos?.map((item) => (
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${item.video_id}`}
+                width={400}
+                height={250}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
